c4503e1d367e94e1e7c30513a37b777baac67085
reaksmey <reaksmey@reaksmey-Aspire-V5-471G.(none)>
2013-04-01 09:45:22 +0700

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myheroku::Application.config.secret_token = '11781a5868bca4e554ca67d3965bf2313271c17e72943298c8f455c05a955a1f6efe4cb91b721f9b8c527705b3731d0d0d3f2f8b4730d0747ec2c1dbe9d8459c'
